cedb206846237ff70f62a14923a9c2854d5d0c9c
Claas <claas@claas.dev>
2025-03-24 17:47:36 +0000

    Add example code

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from openai import OpenAI
+
+
+QUESTION = "What is the capital of France?"
+SYSTEM_PROMPT = "You are a helpful assistant that can answer quesions."
+MODEL = "gpt-4o"
+
+
 def main():
-    print("Hello from ellellehm!")
+    print("Starting ellellehm!")
+    load_dotenv()
+
+    # This is the default and can be omitted
+    api_key = os.environ.get("OPENAI_API_KEY")
+    client = OpenAI(api_key=api_key)
+    messages = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": QUESTION}
+    ]
+
+    # response = client.responses.create(
+    #     model="gpt-4o",
+    #     instructions="You are a coding assistant that talks like a pirate.",
+    #     input="How do I check if a Python object is an instance of a class?",
+    # )
+    # print(response.output_text)
+
+    response = client.chat.completions.create(model=MODEL, messages=messages)
+    text = response.choices[0].message.content
+    print("Response:", text)
+    print("Completed ellellehm")
 
 
 if __name__ == "__main__":
